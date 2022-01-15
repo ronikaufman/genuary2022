5be26d9159d7e091b0c2be58a771478b0e9eeb77
Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>
2022-01-15 18:10:42 +0100

    JAN15 updated

--- JAN11-20/JAN15/sketch.js
+++ JAN11-20/JAN15/sketch.js
@@ -9,29 +9,31 @@ https://ronikaufman.github.io
 let margin = 50, n = 64;
 let world = [];
 let palette = ["#f2eb8a", "#fed000", "#fc8405", "#ed361a", "#e2f0f3", "#b3dce0", "#4464a1", "#203051", "#ffc5c7", "#f398c3", "#cf3895", "#6d358a", "#06b4b0", "#4b8a5f"];
-let col = 0;
+let col = 0, backCol;
 
 function setup() {
   createCanvas(500, 500);
-  background(255);
   noStroke();
+
+  shuffle(palette, true);
+  backCol = palette.pop();
 
   for (let i = 0; i < n; i++) {
     let line = [];
     for (let j = 0; j < n; j++) {
-      line.push(255);
+      line.push(backCol);
     }
     world.push(line);
   }
 
-  shuffle(palette, true);
+  background(255);
 }
 
 function draw() {
   oneStep();
   drawWorld();
 
-  let x = width/2 + (noise(frameCount/150)-0.5)*(width-2*margin)*1.1;
+  let x = width/2 + (noise(frameCount/150)-0.5)*(width-2*margin)*1.5;
   if (frameCount < 3000) addGrain(x, margin);
   if (frameCount % 80 == 0) col++;
 }
@@ -40,15 +42,15 @@ function oneStep() {
   for (let i = n-2; i > -1; i--) {
     for (let j = 0; j < n; j++) {
       let v = world[i][j];
-      if (v != 255) {
-        if (world[i+1][j] == 255) {
-          world[i][j] = 255;
+      if (v != backCol) {
+        if (world[i+1][j] == backCol) {
+          world[i][j] = backCol;
           world[i+1][j] = v;
-        } else if (j > 0 && world[i+1][j-1] == 255) {
-          world[i][j] = 255;
+        } else if (j > 0 && world[i+1][j-1] == backCol) {
+          world[i][j] = backCol;
           world[i+1][j-1] = v;
-        } else if (j < n-1 && world[i+1][j+1] == 255) {
-          world[i][j] = 255;
+        } else if (j < n-1 && world[i+1][j+1] == backCol) {
+          world[i][j] = backCol;
           world[i+1][j+1] = v;
         }
       }
